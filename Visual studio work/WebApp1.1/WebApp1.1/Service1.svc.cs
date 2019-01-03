e6f493b7ca2b9766774edd5fbdd866c9dfa40db8
palakthakur462@gmail.com <palakthakur462@gmail.com>
2019-01-03 16:23:18 +0530

    second commit

--- Visual studio work/WebApp1.1/WebApp1.1/Service1.svc.cs
+++ Visual studio work/WebApp1.1/WebApp1.1/Service1.svc.cs
@@ -11,6 +11,8 @@ namespace WebApp1._1
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // Note: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
+
+        //my firstever comments
     {
         public string GetData(int value)
         {
